26bb0d3397824542cbebdfce5990acd93a1dd415
WYH <1311737763@qq.com>
2023-01-28 17:11:07 +0800

    hot-fix Second Commit

--- src/main/java/com/wyh/gitdemo/DemoApplication.java
+++ src/main/java/com/wyh/gitdemo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         System.out.println("hello Git");
         System.out.println("wyhganqioujiayi");
         System.out.println("wyhganzhuoyaqi");
+        System.out.println("hot-fix qioujiayishiwyhdrbq");
         SpringApplication.run(DemoApplication.class, args);
     }
 
